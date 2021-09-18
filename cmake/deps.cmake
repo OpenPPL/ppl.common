7ddbddb0aa448f6eef361513a5b2cb61207e102b
ouguoyu <ouguoyu@sensetime.com>
2021-09-18 16:39:50 +0800

    download 3rdparty packages instead of using git

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -9,7 +9,7 @@ set(FETCHCONTENT_QUIET OFF)
 
 FetchContent_Declare(hpcc
     GIT_REPOSITORY https://github.com/openppl-public/hpcc.git
-    GIT_TAG v0.1.4
+    GIT_TAG v0.1.5
     GIT_SHALLOW TRUE
     SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
     BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
@@ -29,9 +29,9 @@ set(PYBIND11_TEST OFF CACHE BOOL "disable pybind11 tests")
 set(PYBIND11_NOPYTHON ON CACHE BOOL "do not find python")
 set(PYBIND11_FINDPYTHON OFF CACHE BOOL "do not find python")
 
-hpcc_declare_git_dep(pybind11
-    https://github.com/pybind/pybind11.git
-    v2.7.0)
+hpcc_declare_pkg_dep(pybind11
+    https://github.com/pybind/pybind11/archive/refs/tags/v2.7.0.zip
+    267807f790ef598ef912a79aceefdc10)
 
 # --------------------------------------------------------------------------- #
 
@@ -46,15 +46,15 @@ hpcc_declare_git_dep(luacpp
 set(INSTALL_GTEST OFF CACHE BOOL "")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
 
-hpcc_declare_git_dep(googletest
-    https://github.com/google/googletest.git
-    release-1.10.0)
+hpcc_declare_pkg_dep(googletest
+    https://github.com/google/googletest/archive/refs/tags/release-1.10.0.zip
+    82358affdd7ab94854c8ee73a180fc53)
 
 # --------------------------------------------------------------------------- #
 
 set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "disable benchmark tests")
 set(BENCHMARK_ENABLE_INSTALL OFF CACHE BOOL "")
 
-hpcc_declare_git_dep(benchmark
-    https://github.com/google/benchmark.git
-    v1.5.5)
+hpcc_declare_pkg_dep(benchmark
+    https://github.com/google/benchmark/archive/refs/tags/v1.5.5.zip
+    a63769bd25b5c924b5f4210603fec191)
