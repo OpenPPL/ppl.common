697d4217c4310ab5ace08f1f8043bb24a4747a68
ouguoyu <ouguoyu@sensetime.com>
2021-11-10 12:46:11 +0800

    [build] update deps

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -9,10 +9,8 @@ set(FETCHCONTENT_QUIET OFF)
 
 FetchContent_Declare(hpcc
     GIT_REPOSITORY https://github.com/openppl-public/hpcc.git
-    GIT_TAG 072f718bf47a893c86e48d3a78d2a0f181adb8c5
+    GIT_TAG f5cc50c66fc179acb561a0d2cde75d635f829c56
     SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
-    SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild
     UPDATE_DISCONNECTED True)
 
 FetchContent_GetProperties(hpcc)
@@ -29,16 +27,16 @@ set(PYBIND11_NOPYTHON ON CACHE BOOL "do not find python")
 set(PYBIND11_FINDPYTHON OFF CACHE BOOL "do not find python")
 
 hpcc_declare_pkg_dep(pybind11
-    https://github.com/pybind/pybind11/archive/refs/tags/v2.7.0.zip
-    267807f790ef598ef912a79aceefdc10)
+    https://github.com/pybind/pybind11/archive/refs/tags/v2.7.1.zip
+    cc6d9f0c21694e7c4ec4a00f077de61b)
 
 # --------------------------------------------------------------------------- #
 
 set(LUACPP_INSTALL OFF CACHE BOOL "")
 
 hpcc_declare_pkg_dep(luacpp
-    https://github.com/ouonline/lua-cpp/archive/30d40b248e25e2c72a552c70fde48a289365b9ea.zip
-    1c7e50a374d5e7b6c2a697f3f2e9f765)
+    https://github.com/ouonline/lua-cpp/archive/6074d360820af5f1459f39b1a731b788be0643a0.zip
+    401fdb315bfb3863f789afdd1296084d)
 
 # --------------------------------------------------------------------------- #
 
