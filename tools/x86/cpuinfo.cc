e039bc7a8ffb997061b6ddffdfa84f6e8da027c3
alcanderian <alcanderian@gmail.com>
2022-01-07 16:50:43 +0800

    [x86][feature] add AVX512VNNI detect

--- tools/x86/cpuinfo.cc
+++ tools/x86/cpuinfo.cc
@@ -25,6 +25,7 @@ int main () {
     ppl::common::GetCPUInfoByRun(&info_run);
 
     LOG(INFO) << "Info From CPUID:";
+    LOG(INFO) << "VNNI  : " << (bool)(info_id.isa & ppl::common::ISA_X86_AVX512VNNI);
     LOG(INFO) << "AVX512: " << (bool)(info_id.isa & ppl::common::ISA_X86_AVX512);
     LOG(INFO) << "AVX2  : " << (bool)(info_id.isa & ppl::common::ISA_X86_AVX2);
     LOG(INFO) << "FMA3  : " << (bool)(info_id.isa & ppl::common::ISA_X86_FMA);
@@ -35,6 +36,7 @@ int main () {
     LOG(INFO) << "L3 Cahce: " << info_id.l3_cache_size;
 
     LOG(INFO) << "Info From RUN:";
+    LOG(INFO) << "VNNI  : " << (bool)(info_run.isa & ppl::common::ISA_X86_AVX512VNNI);
     LOG(INFO) << "AVX512: " << (bool)(info_run.isa & ppl::common::ISA_X86_AVX512);
     LOG(INFO) << "AVX2  : " << (bool)(info_run.isa & ppl::common::ISA_X86_AVX2);
     LOG(INFO) << "FMA3  : " << (bool)(info_run.isa & ppl::common::ISA_X86_FMA);
