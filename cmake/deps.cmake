af2d3683f3816895675dd8f347ac42ce53f3130b
ouguoyu <ouguoyu@sensetime.com>
2022-07-22 17:21:08 +0800

    use git for downloading deps by default

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -13,7 +13,7 @@ endif()
 
 # --------------------------------------------------------------------------- #
 
-set(__HPCC_COMMIT__ 614c481aa9b8ff949ddfba70a3ae1aefa39ffd58)
+set(__HPCC_COMMIT__ e6e445437fba399cc86867536fdf4252bbefae61)
 
 if(PPLCOMMON_DEP_HPCC_PKG)
     FetchContent_Declare(hpcc
@@ -21,16 +21,13 @@ if(PPLCOMMON_DEP_HPCC_PKG)
         SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
         BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
         SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
-elseif(PPLCOMMON_DEP_HPCC_GIT)
+else()
+    if(NOT PPLCOMMON_DEP_HPCC_GIT)
+        set(PPLCOMMON_DEP_HPCC_GIT "https://github.com/openppl-public/hpcc.git")
+    endif()
     FetchContent_Declare(hpcc
         GIT_REPOSITORY ${PPLCOMMON_DEP_HPCC_GIT}
         GIT_TAG ${__HPCC_COMMIT__}
-        SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
-        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
-        SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
-else()
-    FetchContent_Declare(hpcc
-        URL https://github.com/openppl-public/hpcc/archive/${__HPCC_COMMIT__}.zip
         SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
         BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
         SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
@@ -56,13 +53,13 @@ set(__PYBIND11_TAG__ v2.9.2)
 if(PPLCOMMON_DEP_PYBIND11_PKG)
     hpcc_declare_pkg_dep(pybind11
         ${PPLCOMMON_DEP_PYBIND11_PKG})
-elseif(PPLCOMMON_DEP_PYBIND11_GIT)
+else()
+    if(NOT PPLCOMMON_DEP_PYBIND11_GIT)
+        set(PPLCOMMON_DEP_PYBIND11_GIT "https://github.com/pybind/pybind11.git")
+    endif()
     hpcc_declare_git_dep(pybind11
         ${PPLCOMMON_DEP_PYBIND11_GIT}
         ${__PYBIND11_TAG__})
-else()
-    hpcc_declare_pkg_dep(pybind11
-        https://github.com/pybind/pybind11/archive/refs/tags/${__PYBIND11_TAG__}.zip)
 endif()
 
 unset(__PYBIND11_TAG__)
@@ -77,13 +74,13 @@ set(__LUACPP_COMMIT__ f381a4702017b61ee9662ae9fa7bceec8b5c7b32)
 if(PPLCOMMON_DEP_LUACPP_PKG)
     hpcc_declare_pkg_dep(luacpp
         ${PPLCOMMON_DEP_LUACPP_PKG})
-elseif(PPLCOMMON_DEP_LUACPP_GIT)
+else()
+    if(NOT PPLCOMMON_DEP_LUACPP_GIT)
+        set(PPLCOMMON_DEP_LUACPP_GIT "https://github.com/ouonline/lua-cpp.git")
+    endif()
     hpcc_declare_git_dep(luacpp
         ${PPLCOMMON_DEP_LUACPP_GIT}
         ${__LUACPP_COMMIT__})
-else()
-    hpcc_declare_pkg_dep(luacpp
-        https://github.com/ouonline/lua-cpp/archive/${__LUACPP_COMMIT__}.zip)
 endif()
 
 unset(__LUACPP_COMMIT__)
@@ -98,13 +95,13 @@ set(__GOOGLETEST_TAG__ release-1.10.0)
 if(PPLCOMMON_DEP_GOOGLETEST_PKG)
     hpcc_declare_pkg_dep(googletest
         ${PPLCOMMON_DEP_GOOGLETEST_PKG})
-elseif(PPLCOMMON_DEP_GOOGLETEST_GIT)
+else()
+    if(NOT PPLCOMMON_DEP_GOOGLETEST_GIT)
+        set(PPLCOMMON_DEP_GOOGLETEST_GIT "https://github.com/google/googletest.git")
+    endif()
     hpcc_declare_git_dep(googletest
         ${PPLCOMMON_DEP_GOOGLETEST_GIT}
         ${__GOOGLETEST_TAG__})
-else()
-    hpcc_declare_pkg_dep(googletest
-        https://github.com/google/googletest/archive/refs/tags/${__GOOGLETEST_TAG__}.zip)
 endif()
 
 unset(__GOOGLETEST_TAG__)
@@ -119,13 +116,13 @@ set(__BENCHMARK__TAG__ v1.5.6)
 if(PPLCOMMON_DEP_BENCHMARK_PKG)
     hpcc_declare_pkg_dep(benchmark
         ${PPLCOMMON_DEP_BENCHMARK_PKG})
-elseif(PPLCOMMON_DEP_BENCHMARK_GIT)
+else()
+    if(NOT PPLCOMMON_DEP_BENCHMARK_GIT)
+        set(PPLCOMMON_DEP_BENCHMARK_GIT "https://github.com/google/benchmark.git")
+    endif()
     hpcc_declare_git_dep(benchmark
         ${PPLCOMMON_DEP_BENCHMARK_GIT}
         ${__BENCHMARK__TAG__})
-else()
-    hpcc_declare_pkg_dep(benchmark
-        https://github.com/google/benchmark/archive/refs/tags/${__BENCHMARK__TAG__}.zip)
 endif()
 
 unset(__BENCHMARK__TAG__)
