1f9dcfb29606029d11d4f772fb012b1debd1c8a0
ouguoyu <ouguoyu@sensetime.com>
2022-09-06 16:37:34 +0800

    [build] export lib name based on the target os

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -11,8 +11,14 @@ install(FILES ${PPLCOMMON_PARAM_HEADERS}
 install(TARGETS pplcommon_static DESTINATION lib)
 
 set(PPLCOMMON_CMAKE_CONFIG_FILE ${CMAKE_CURRENT_BINARY_DIR}/generated/pplcommon-config.cmake)
+if(MSVC)
+    set(__PPLCOMMON_LIB_NAME__ "pplcommon_static.lib")
+else()
+    set(__PPLCOMMON_LIB_NAME__ "libpplcommon_static.a")
+endif()
 configure_file(cmake/pplcommon-config.cmake.in
     ${PPLCOMMON_CMAKE_CONFIG_FILE}
     @ONLY)
+unset(__PPLCOMMON_LIB_NAME__)
 install(FILES ${PPLCOMMON_CMAKE_CONFIG_FILE} DESTINATION lib/cmake/ppl)
 unset(PPLCOMMON_CMAKE_CONFIG_FILE)
