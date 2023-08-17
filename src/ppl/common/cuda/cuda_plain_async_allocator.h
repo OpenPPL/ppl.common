3bbd21722a2194e1302989fc26a10c677bda4c28
ouguoyu <ouguoyu@sensetime.com>
2023-08-17 19:20:36 +0800

    add CudaPlainAsyncAllocator

---
+++ src/ppl/common/cuda/cuda_plain_async_allocator.h
@@ -0,0 +1,46 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#ifndef _ST_HPC_PPL_COMMON_CUDA_PLAIN_ASYNC_ALLOCATOR_H_
+#define _ST_HPC_PPL_COMMON_CUDA_PLAIN_ASYNC_ALLOCATOR_H_
+
+#include "ppl/common/allocator.h"
+#include <cuda_runtime_api.h>
+
+namespace ppl { namespace common {
+
+class CudaPlainAsyncAllocator final : public Allocator {
+public:
+    void Init(cudaStream_t stream) {
+        stream_ = stream;
+    }
+    void* Alloc(uint64_t size) override {
+        void* ptr = nullptr;
+        cudaMallocAsync(&ptr, size, stream_);
+        return ptr;
+    }
+    void Free(void* ptr) override {
+        cudaFreeAsync(ptr, stream_);
+    }
+
+private:
+    cudaStream_t stream_ = 0;
+};
+
+}}
+
+#endif
