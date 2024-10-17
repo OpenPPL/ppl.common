7b7d9ed1795b7547ee63c37ac095a38ef9a8b445
jqf <jiangqingfeng1@sensetime.com>
2024-10-17 13:57:48 +0800

    Modified framechain.cc to add openCL version higher than 200 judgment

--- src/ppl/common/ocl/framechain.cc
+++ src/ppl/common/ocl/framechain.cc
@@ -323,7 +323,13 @@ bool FrameChain::ifSupportQcomHints(){
 
 void FrameChain::get_extention_info() {
     char ext_info_str[MAX_EXT_CHAR_LENGTH];
-
+    // add by jiangqingfeng 2024.10.16
+    Device* device = getSharedDevice();
+    int opencl_version = device->getOpenCLVersion();
+    if (opencl_version<200){
+        LOG(ERROR) << " The OpenCL version is too old, Please update. opencl_version:" << opencl_version;
+    }
+    // add by jiangqingfeng 2024.10.16
     compile_options_ext_defaults_ = " ";
 
     cl_int err = clGetDeviceInfo(device_id_, CL_DEVICE_EXTENSIONS, MAX_EXT_CHAR_LENGTH, ext_info_str, nullptr);
